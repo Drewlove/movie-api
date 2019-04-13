527ee878920c80753aaf223ee23e7e89c29e9d47
Drew Love <davidandrewlove84@gmail.com>
2019-04-13 18:46:34 -0400

    movie api

---
+++ server.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const app = express()
+app.use(morgan('dev'));
+app.use(helmet()); 
+app.use(cors()); 
+
+const movieDB = require('./movieDB')
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN; 
+    const authToken = req.get('Authorization').split(' ')[1]
+    if(apiToken !== authToken || !authToken){
+        res.json('must have valid authorization')
+    }
+    next()
+})
+
+app.get('/movies', function sortByGenre(req, res){
+    const{avgVote, country, genre} = req.query; 
+    let results = movieDB; 
+    if(avgVote){
+        results = movieDB.filter(movie => movie.avg_vote >= parseFloat(avgVote)); 
+    }
+    if(country){
+        results = movieDB.filter(movie => 
+            movie.country.toLowerCase() === country.toLowerCase())
+    }
+    if(genre){
+        results = movieDB.filter(movie => 
+            movie.genre.toLowerCase() === genre.toLowerCase())
+        }
+    res.json(results)
+})
+
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(function validateBearerToken(req, res, next) {
+//     const apiToken = process.env.API_TOKEN
+//     console.log('test works', apiToken)
+//     // move to the next middleware
+//     next()
+//   })
+
+
+
